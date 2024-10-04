c4a50f8cbb3078f88db4448ed00d46cca22344ec
Jamie <78187683+ItsJamie20@users.noreply.github.com>
2024-10-04 13:17:22 +1000

    Initial commit

---
+++ Router.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\core;
+
+use app\core\exception\NotFoundException;
+
+class Router{
+
+    public Request $request;
+    public Response $response;
+    protected array $routes = [];
+
+    public function __construct(Request $request, Response $response){
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function get($path, $callback){
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function post($path, $callback){
+        $this->routes['post'][$path] = $callback;
+    }
+
+    public function resolve(){
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if(!$callback) {
+            throw new NotFoundException();
+            exit;
+        }
+        if(is_string($callback)){
+            echo 'isString';
+            return  Application::$app->view->renderView($callback);
+        }
+        if(is_array($callback)){
+            Application::$app->controller = new $callback[0]();
+            $controller = Application::$app->controller;
+            $controller->action = $callback[1];
+            foreach($controller->getMiddlewares() as $middleware){
+                $middleware->execute();
+            }
+            $callback[0] = $controller;
+        }
+        return call_user_func($callback, $this->request, $this->response);
+    }
+
+}
